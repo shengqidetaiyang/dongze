323a234d9ee060a548cc9da540d8b26d09137a9c
shengqidetaiyang <494407127@qq.com>
2017-03-15 11:24:32 +0800

    add information page
    增加资讯页

--- js/show/index.js
+++ js/show/index.js
@@ -2,7 +2,7 @@
 * @Author: Marte
 * @Date:   2017-03-04 10:49:22
 * @Last Modified by:   Marte
-* @Last Modified time: 2017-03-13 17:40:09
+* @Last Modified time: 2017-03-15 10:35:19
 */
 
 $(document).ready(function(){
@@ -18,20 +18,6 @@ $(document).ready(function(){
         prevButton: '.swiper-button-prev',
         
       })   
-      $(".commHeader .top .third").bind('click',  function(event) {
-          /* Act on the event */
-          $(this).find("span").hide();
-          $(this).find("ul").show();
-      });
-      $(".commHeader .top .third ul").bind('click',  function(event) {
-          /* Act on the event */
-          var e = event || window.event;
-          var target = e.target.innerText;
-          $(this).hide();
-          $(this).parent().find("span").show();
-          e.stopPropagation();
-          $(this).parent().find("span").text(target);
-      });
       $(".content .smallTab .right .choose").each( function(index,ele) {
          $(ele).bind('click',function(event){
           var e = event || window.event;
@@ -81,40 +67,4 @@ $(document).ready(function(){
            left: -400*index + "px"
         })
       })
-
-    $(".fix .fifth").on("click",function(){
-        var speed=600;//滑动的速度
-        $('body,html').animate({ scrollTop: 0 }, speed);
-        $(".fixedTop").slideUp();   
-        return false;
-    })
-  var EventUtil={
-      getEvent:function(event){return event?event:window.event;},
-          //滚轮事件对象的 wheelDelta/FF DOMMouseScroll
-      getWheelDelta:function(event){
-          if(event.wheelDelta){//ff以外的浏览器
-              //在最新版的opera中window返回undefined ， 在opera9.5中返回对象 在9.5版本之前的版本中wheelDelta的正负号颠倒的
-              return (window.opera&&window.opera.version()<9.5?-event.wheelDelta:event.wheelDelta);
-              }else{return -event.detail*40;}//ff
-          },
-      //事件处理程序
-      addHandler:function(element,type,handler){
-          if(element.addEventListener){element.addEventListener(type,handler,false)}//DOM2
-          else if(element.attachEvent){element.attachEvent('on'+type,handler);}//ie
-          else{element['on'+type]=handler;}//DOM0
-          }
-  }
-  var a=document.getElementById('a');
-  EventUtil.addHandler(document,'mousewheel',handleMouseWheel);//注册ie的滚轮事件
-  EventUtil.addHandler(document,'DOMMouseScroll',handleMouseWheel);//注册ff的滚轮事件
-  function handleMouseWheel(e){
-      if($("body").scrollTop()>= 150){
-        $(".fixedTop").slideDown();
-      }
-      else{
-        $(".fixedTop").slideUp();      
-      }
-  }
- 
-    
 });
--- js/show/list.js
+++ js/show/list.js
@@ -2,7 +2,7 @@
 * @Author: Marte
 * @Date:   2017-03-07 15:33:22
 * @Last Modified by:   Marte
-* @Last Modified time: 2017-03-13 17:39:42
+* @Last Modified time: 2017-03-15 10:32:17
 */
 
 $(document).ready(function(){
@@ -47,37 +47,4 @@ $(document).ready(function(){
     var index = getPara("style");
     $(".nav li a").eq(index).addClass('active');
     $(".fixedTop li a").eq(index).addClass('active');
-    $(".fix .fifth").on("click",function(){
-        var speed=600;//滑动的速度
-        $('body,html').animate({ scrollTop: 0 }, speed);
-        $(".fixedTop").slideUp();   
-        return false;
-    })
-    var EventUtil={
-        getEvent:function(event){return event?event:window.event;},
-            //滚轮事件对象的 wheelDelta/FF DOMMouseScroll
-        getWheelDelta:function(event){
-            if(event.wheelDelta){//ff以外的浏览器
-                //在最新版的opera中window返回undefined ， 在opera9.5中返回对象 在9.5版本之前的版本中wheelDelta的正负号颠倒的
-                return (window.opera&&window.opera.version()<9.5?-event.wheelDelta:event.wheelDelta);
-                }else{return -event.detail*40;}//ff
-            },
-        //事件处理程序
-        addHandler:function(element,type,handler){
-            if(element.addEventListener){element.addEventListener(type,handler,false)}//DOM2
-            else if(element.attachEvent){element.attachEvent('on'+type,handler);}//ie
-            else{element['on'+type]=handler;}//DOM0
-            }
-    }
-    var a=document.getElementById('a');
-    EventUtil.addHandler(document,'mousewheel',handleMouseWheel);//注册ie的滚轮事件
-    EventUtil.addHandler(document,'DOMMouseScroll',handleMouseWheel);//注册ff的滚轮事件
-    function handleMouseWheel(e){
-          if($("body").scrollTop()>= 150){
-            $(".fixedTop").slideDown();
-          }
-          else{
-            $(".fixedTop").slideUp();      
-          }
-      }
 });
